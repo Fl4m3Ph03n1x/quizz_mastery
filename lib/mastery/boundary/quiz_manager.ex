a5670716cb9a83aa028ee1f6d4f45616223913e5
Pedro Martins <pedroma@BCNPEDMARTMAC.local>
2019-10-21 14:44:15 +0200

    added quiz manager and sessions

---
+++ lib/mastery/boundary/quiz_manager.ex
@@ -0,0 +1,56 @@
+defmodule Mastery.Boundary.QuizManager do
+
+  use GenServer
+
+  alias Mastery.Core.Quiz
+
+  @type quizzes :: %{String.t => Quiz.t}
+
+  ##############
+  # Public API #
+  ##############
+
+  @spec build_quiz(module, keyword) :: :ok
+  def build_quiz(manager \\ __MODULE__, quiz_fields), do:
+    GenServer.call(manager, {:build_quiz, quiz_fields})
+
+  @spec add_template(module, String.t, keyword) :: :ok
+  def add_template(manager \\ __MODULE__, quiz_title, template_fields), do:
+    GenServer.call(manager, {:add_template, quiz_title, template_fields})
+
+  @spec lookup_quiz_by_title(module, String.t) :: Quiz.t
+  def lookup_quiz_by_title(manager \\ __MODULE__, quiz_title), do:
+    GenServer.call(manager, {:lookup_quiz_by_title, quiz_title})
+
+  ############################
+  # Callback implementations #
+  ############################
+
+  @impl GenServer
+  @spec init(any) :: {:stop, String.t} | {:ok, quizzes}
+  def init(quizzes) when is_map(quizzes), do: {:ok, quizzes}
+  def init(_quizzes), do: {:stop, "quizzes must be a map"}
+
+  @impl GenServer
+  def handle_call({:build_quiz, quiz_fields}, _from, quizzes) do
+    quiz = Quiz.new(quiz_fields)
+
+    # Using a title as UUID, isn't this dangerous for clashes?
+    new_quizzes = Map.put(quizzes, quiz.title, quiz)
+    {:reply, :ok, new_quizzes}
+  end
+
+  def handle_call(
+    {:add_template, quiz_title, template_fields}, _from, quizzes
+  ) do
+    new_quizzes = Map.update!(
+      quizzes,
+      quiz_title,
+      fn quiz -> Quiz.add_template(quiz, template_fields) end
+    )
+    {:reply, :ok, new_quizzes}
+  end
+
+  def handle_call({:lookup_quiz_by_title, quiz_title}, _from, quizzes), do:
+    {:reply, quizzes[quiz_title], quizzes}
+end
---
+++ lib/mastery/boundary/quiz_session.ex
@@ -0,0 +1,54 @@
+defmodule Mastery.Boundary.QuizSession do
+
+  use GenServer
+
+  alias Mastery.Core.{Quiz, Response}
+
+  @type state :: {Quiz.t, String.t}
+
+  ##############
+  # Public API #
+  ##############
+
+  @spec select_question(module) :: String.t
+  def select_question(session), do:
+    GenServer.call(session, :select_question)
+
+  @spec answer_question(module, String.t) :: :finished | {String.t, boolean}
+  def answer_question(session, answer), do:
+    GenServer.call(session, {:answer_question, answer})
+
+  ############################
+  # Callback implementations #
+  ############################
+
+  @impl GenServer
+  @spec init(state) :: {:ok, state}
+  def init({quiz, email}), do: {:ok, {quiz, email}}
+
+  @impl GenServer
+  def handle_call(:select_question, _from, {quiz, email}) do
+    quiz = Quiz.select_question(quiz)
+    {:reply, quiz.current_question.asked, {quiz, email}}
+  end
+
+  def handle_call({:answer_question, answer}, _from, {quiz, email}) do
+    quiz
+    |> Quiz.answer_question(Response.new(quiz, email, answer))
+    |> Quiz.select_question()
+    |> maybe_finish(email)
+  end
+
+  @spec maybe_finish(Quiz.t | nil, String.t) ::
+    {:stop, :normal, :finished, nil}
+    | {:reply, {String.t, boolean}, state}
+  defp maybe_finish(nil, _email), do: {:stop, :normal, :finished, nil}
+
+  defp maybe_finish(quiz, email), do:
+    {
+      :reply,
+      {quiz.current_question.asked, quiz.last_response.correct},
+      {quiz, email}
+    }
+
+end
