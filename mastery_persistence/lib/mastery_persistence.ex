8bd6414ea70657adfcab3008bf7656fb414cd1b5
Pedro Martins <palmelapedro@gmail.com>
2020-02-03 10:24:19 +0100

    Adding Persistence layer

---
+++ mastery_persistence/lib/mastery_persistence.ex
@@ -0,0 +1,30 @@
+defmodule MasteryPersistence do
+  @moduledoc """
+  Documentation for MasteryPersistence.
+  """
+
+  import Ecto.Query, only: [from: 2]
+
+  alias MasteryPersistence.{Response, Repo}
+
+  def record_response(response, in_transaction \\ fn _response -> :ok end) do
+    {:ok, result} = Repo.transaction(fn -> 
+      %{
+        quiz_title:     to_string(response.quiz_title),
+        template_name:  to_string(response.template_name),
+        to:             response.to,
+        email:          response.email,
+        answer:         response.answer,
+        correct:        response.correct,
+        inserted_at:    response.timestamp,
+        updated_at:     response.timestamp
+      }
+      |> Response.record_changeset()
+      |> Repo.insert!()
+
+      in_transaction.(response)
+    end)
+
+    result
+  end
+end
---
+++ mastery_persistence/mix.exs
@@ -0,0 +1,28 @@
+defmodule MasteryPersistence.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :mastery_persistence,
+      version: "0.1.0",
+      elixir: "~> 1.9",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {MasteryPersistence.Application, []}
+    ]
+  end
+
+  defp deps do
+    [
+      {:ecto_sql, "~> 3.1"},
+      {:postgrex, "~> 0.14.1"}
+    ]
+  end
+end
